d210a88201036e16fea02a83d4f20747f51052c5
jonathanchen1999 <57915885+jonathanchen1999@users.noreply.github.com>
2020-03-10 13:29:58 -0400

    lab_5

--- lab_5/main.js
+++ lab_5/main.js
@@ -1,1 +1,20 @@
-const myContent=document.querySelector('.content');
+function createList() {
+    const myHeader =document.querySelector('.header');
+    myHeader.textContent="Jonathan Chen's list of countries";
+
+    const myContent= document.querySelector('.content');
+    myContent.removeChild(myContent.lastElementChild);
+    var myList = document.createElement('ol');
+    myList.classList.add("countries");
+    myContent.appendChild(myList);
+    var countryList =
+    
+
+}
+
+function randomCountries(){ 
+
+}
+
+
+
